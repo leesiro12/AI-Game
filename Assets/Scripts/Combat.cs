c7d3077805ae4e222c375e22b237f3178a7001e8
leesiro12 <phamducminh.1901@gmail.com>
2023-10-18 17:52:50 +0700

    Initial commit

---
+++ Assets/Scripts/Combat.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat : MonoBehaviour
+{
+    public Renderer ren;
+    public Material[] mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartCoroutine(Hit());
+        }
+        
+    }
+
+    IEnumerator Hit()
+    {
+        ren.material = mat[0];
+        yield return new WaitForSeconds(0.3f);
+        ren.material = mat[1];
+    }
+}
